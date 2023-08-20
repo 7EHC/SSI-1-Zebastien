b93a069f9d9edea56ac91517ca28b4216e50c50d
Purin <purin.peera@mail.kmutt.ac.th>
2023-08-20 20:53:26 +0700

    update create&insert table

--- DBScripts/createTableUser.sql
+++ DBScripts/createTableUser.sql
@@ -1,25 +1,12 @@
 use mydb;
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
-CREATE TABLE if not exists users(
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `name` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(150),
-  `role` ENUM('admin', 'announcer') NOT NULL DEFAULT 'announcer',
-  `createdOn` DATETIME NOT NULL,
-  `updatedOn` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+create table users (
+	id int auto_increment primary key,
+    username varchar(45) unique,
+    name varchar(100) unique,
+    email varchar(150) unique,
+    role enum('admin', 'announcer'),
+    createdOn datetime,
+    updatedOn datetime
+);
 
