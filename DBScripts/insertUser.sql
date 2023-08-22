9e95aa0a20c76db9c31f636751a9be520ca82a9b
Purin <purin.peera@mail.kmutt.ac.th>
2023-08-22 17:07:56 +0700

    edit aj.sanit surname

--- DBScripts/insertUser.sql
+++ DBScripts/insertUser.sql
@@ -1,6 +1,6 @@
 use mydb; 
 
-insert into users values(1,'sanit', 'Sanit Sirisawatvatan', 'sanit.sir@kmutt.ac.th', 'admin', '2023-08-15 08:00:00+07:00', '2023-08-15 08:00:00+07:00');
+insert into users values(1,'sanit', 'Sanit Sirisawatvatana', 'sanit.sir@kmutt.ac.th', 'admin', '2023-08-15 08:00:00+07:00', '2023-08-15 08:00:00+07:00');
 insert into users values(2,'pornthip', 'Pornthip Sirijutikul', 'pornthip.sri@kmutt.ac.th', 'announcer', '2023-08-15 09:30:00+07:00', '2023-08-15 09:30:00+07:00');
 insert into users values(3,'jaruwan_w', 'Jaruwan Maneesart', 'jaruwan.wee@kmutt.ac.th', 'announcer', '2023-08-16 08:00:00+07:00', '2023-08-16 08:00:00+07:00');
 insert into users values(4,'vichchuda', 'Vichchuda Tedoloh', 'vichchuda.ted@kmutt.ac.th', 'announcer', '2023-08-16 09:30:00+07:00', '2023-08-16 09:30:00+07:00');
