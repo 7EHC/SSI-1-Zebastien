420bb07c3335c976150baf3a96e192abb9834014
Purin <purin.peera@mail.kmutt.ac.th>
2023-08-23 00:07:18 +0700

    update user edit

--- Back-end/src/main/java/sit/project/projectv1/controllers/UserController.java
+++ Back-end/src/main/java/sit/project/projectv1/controllers/UserController.java
@@ -1,11 +1,15 @@
 package sit.project.projectv1.controllers;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import sit.project.projectv1.dtos.AddAnnouncementDTO;
 import sit.project.projectv1.dtos.InputUserDTO;
+import sit.project.projectv1.dtos.OutputAnnouncementDTO;
 import sit.project.projectv1.dtos.OutputUserDTO;
+import sit.project.projectv1.entities.Announcement;
 import sit.project.projectv1.entities.User;
 import sit.project.projectv1.services.UserService;
 import sit.project.projectv1.utils.ListMapper;
@@ -31,11 +35,6 @@ public class UserController {
 
     @PostMapping
     public OutputUserDTO createUser(@Valid @RequestBody InputUserDTO userDTO) {
-
-        userDTO.getName().trim();
-        userDTO.getEmail().trim();
-        userDTO.getUsername().trim();
-
         User user = modelMapper.map(userDTO, User.class);
         user.setId(null);
         userService.createNewUser(user);
@@ -45,5 +44,18 @@ public class UserController {
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Integer id) {
         userService.deleteUser(id);
+    }
+
+    @GetMapping("/{id}")
+    public OutputUserDTO getUserById(@PathVariable Integer id){
+        return modelMapper.map(userService.getUserById(id), OutputUserDTO.class);
+    }
+
+    @PutMapping("/{id}")
+    public OutputUserDTO update(@PathVariable Integer id, @RequestBody InputUserDTO updateUser) {
+        User user = modelMapper.map(updateUser, User.class);
+        user.setId(id);
+        userService.updateUser(id, user);
+        return modelMapper.map(user, OutputUserDTO.class);
     }
 }
