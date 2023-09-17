eea6428db01f6797090a17e38ae96a2a59736164
Purin <purin.peera@mail.kmutt.ac.th>
2023-09-18 01:21:39 +0700

    update create/insert script

--- DBScripts/createTableUser.sql
+++ DBScripts/createTableUser.sql
@@ -7,11 +7,10 @@ SET @@session.time_zone = "+00:00";
 create table users (
 	id int auto_increment primary key,
     username varchar(45) unique,
-    password varchar(100) not null default 'Sasadmin22*',
+    password varchar(100) not null,
     name varchar(100) unique,
     email varchar(150) unique,
     role enum('admin', 'announcer') not null default 'announcer',
     createdOn timestamp not null default current_timestamp,
     updatedOn timestamp not null default current_timestamp on update current_timestamp
 );
-
