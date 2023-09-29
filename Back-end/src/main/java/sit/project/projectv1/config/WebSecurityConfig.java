1ba95d13cee1dc4181967f02ab992d6e6d83792f
Purin <purin.peera@mail.kmutt.ac.th>
2023-09-30 02:34:28 +0700

    update fetch token url

---
+++ Back-end/src/main/java/sit/project/projectv1/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+//package sit.project.projectv1.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class WebSecurityConfig {
+//
+//    private final AuthenticationConfiguration authConfiguration;
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager() throws Exception {
+//        return authConfiguration.getAuthenticationManager();
+//    }
+//}
